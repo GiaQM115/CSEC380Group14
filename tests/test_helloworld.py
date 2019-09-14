195232440963c990549c49af540da5ea807e983b
Gia Mule <gqmule1@gmail.com>
2019-09-14 12:27:40 -0400

    update .travis.yml to reflect appropriate filesystem structure in docker command

--- tests/test_helloworld.py
+++ tests/test_helloworld.py
@@ -1,8 +1,7 @@
 import requests
 import pytests
 
-# intentionally added error to test travis-ci
-def test(:
+def test():
     req = requests.get("http://127.0.0.1")
     assert "Hello World" in req.text
 
