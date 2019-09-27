5dcc563d4aaca7e1a0fd6652bd92716f147bee8f
Conrad Schneggenburger <ccs5486@rit.edu>
2019-09-27 13:06:17 -0400

    Update PHP configuration at large
    + Modify frontend to use php-fcgi configuration
    + Add index.js
    + Update index.html to include index.js import
    + Update page JavaScript

---
+++ docker/frontend/index.js
@@ -0,0 +1,12 @@
+/**
+ * Execute this when the document is done loading
+ */
+window.onload = function () {
+    alert("WELCOME");
+};
+
+/**
+ * Prompt user for login
+ */
+function loginPrompt() {
+}
