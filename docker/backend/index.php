f4468ac34e35633e78bac5ecde0a3f3e13ea4255
Conrad Schneggenburger <ccs5486@rit.edu>
2019-09-29 23:29:57 -0400

    Implement PHP<-->DB test function

--- docker/backend/index.php
+++ docker/backend/index.php
@@ -1,5 +1,14 @@
 <?php
-echo "No junk food, just earthly goods!
-I ate weird berries in the woods.
-Now I'm seeing colors; I'm getting higher.
-I think I'll start... a forest fire!";
+
+function db_connection_test() {
+    $link = mysqli_connect('{host}', '{user}', '{password}', '{db_name}');
+
+    // If connection is not successful you will see text error
+    if (!$link) {
+        die('Could not connect: ' . mysqli_error($link));
+    }
+    // If connection is successfully you will see message below
+    echo 'Connected successfully';
+
+    mysqli_close($link);
+}
