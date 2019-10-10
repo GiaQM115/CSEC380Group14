457cdfc7a63340c88bbbb6d1b42ace0d58fdfdbd
Conrad Schneggenburger <ccs5486@rit.edu>
2019-10-10 16:13:21 -0400

    Update backend with authentication

--- docker/backend/initdb.sql
+++ docker/backend/initdb.sql
@@ -3,19 +3,20 @@ CREATE DATABASE brickflix;
 USE brickflix;
 
 CREATE TABLE account (
-    id INT(8) UNSIGNED NOT NULL AUTO_INCREMENT,
-    login varchar(16) NOT NULL,
-    password varchar(32) NOT NULL,
+    login_id varchar(16) NOT NULL,
+    pass_hash  char(64) NOT NULL,
     email varchar(64),
-    CONSTRAINT pk_id PRIMARY KEY (id)
-) DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+    CONSTRAINT pk_id PRIMARY KEY (login_id)
+) DEFAULT CHARSET=utf8;
 
 CREATE TABLE video (
-    id INT(8) UNSIGNED NOT NULL AUTO_INCREMENT,
-    uploader_id INT(8) UNSIGNED NOT NULL,
+    login_id varchar(16) NOT NULL,
+    uploader_id varchar(16) NOT NULL,
     upload_date DATE NOT NULL DEFAULT '1970-01-01',
-    CONSTRAINT pk_id PRIMARY KEY (id),
-    CONSTRAINT fk_uploader_id FOREIGN KEY (uploader_id) REFERENCES account(id)
+    CONSTRAINT pk_id PRIMARY KEY (login_id),
+    CONSTRAINT fk_uploader_id FOREIGN KEY (uploader_id) REFERENCES account(login_id)
 ) DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
 
-INSERT INTO account (login, password, email) VALUES ('ccs5486', '1MoarRoad2Cr0ss!', 'ccs5486@rit.edu');
+GRANT ALL PRIVILEGES ON brickflix TO 'php'@'localhost' IDENTIFIED BY 'SuperSecretPassword';
+
+INSERT INTO account (login_id, pass_hash, email) VALUES ('ccs5486', SHA2('1MoarRoad2Cr0ss!', 256), 'ccs5486@rit.edu');
