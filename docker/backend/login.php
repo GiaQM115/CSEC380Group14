e8a6a611e7697ba238bc1d3ec26abd7e6047e4bf
Conrad Schneggenburger <ccs5486@rit.edu>
2019-10-11 18:08:29 -0400

    Hotfix backend

--- docker/backend/login.php
+++ docker/backend/login.php
@@ -3,12 +3,12 @@
 session_start();
 
 // Check if the user is already logged in, if yes then redirect them to home page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
+if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
     header("location: home.html");
     exit;
 }
 
-// Include config file
+// Include db_link config file
 require_once "db_link.php";
 
 // Define variables and initialize with empty values
@@ -16,28 +16,27 @@ $username = $password = "";
 $username_err = $password_err = "";
 
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-
+if($_SERVER["REQUEST_METHOD"] == "POST") {
     // Check if username is empty
-    if(empty(trim($_POST["username"]))){
+    if(empty(trim($_POST["username"]))) {
         $username_err = "Please enter username.";
     } else{
         $username = trim($_POST["username"]);
     }
 
     // Check if password is empty
-    if(empty(trim($_POST["password"]))){
+    if(empty(trim($_POST["password"]))) {
         $password_err = "Please enter your password.";
     } else{
         $password = trim($_POST["password"]);
     }
 
     // Validate credentials
-    if(empty($username_err) && empty($password_err)){
+    if(empty($username_err) && empty($password_err)) {
         // Prepare a select statement
         $sql = "SELECT login_id, pass_hash FROM account WHERE login_id = ?";
 
-        if($stmt = mysqli_prepare($link, $sql)){
+        if($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_username);
 
@@ -45,16 +44,16 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             $param_username = $username;
 
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
+            if(mysqli_stmt_execute($stmt)) {
                 // Store result
                 mysqli_stmt_store_result($stmt);
 
                 // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){
+                if(mysqli_stmt_num_rows($stmt) == 1) {
                     // Bind result variables
                     mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
+                    if(mysqli_stmt_fetch($stmt)) {
+                        if(password_verify($password, $hashed_password)) {
                             // Password is correct, so start a new session
                             session_start();
 
@@ -64,17 +63,17 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["username"] = $username;
 
                             // Redirect user to welcome page
-                            header("location: home.html");
-                        } else{
+                            header("Location: home.html");
+                        } else {
                             // Display an error message if password is not valid
                             $password_err = "The password you entered was not valid.";
                         }
                     }
-                } else{
+                } else {
                     // Display an error message if username doesn't exist
                     $username_err = "No account found with that username.";
                 }
-            } else{
+            } else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
         }
