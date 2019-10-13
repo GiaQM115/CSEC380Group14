c69a61c5b83844868b736a6238c09e149595148c
Conrad Schneggenburger <ccs5486@rit.edu>
2019-10-13 17:41:59 -0400

    Overhaul frontend Add favicon.ico Revert index page to index.html instead of login.html Update all references of login.html to index.html Fix HTML spacing inconsistencies

--- docker/frontend/js/login.js
+++ docker/frontend/js/login.js
@@ -13,17 +13,17 @@ function loginAttempt() {
     // we should just tell the user to get a new browswer but whatever
     xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
   }
-  xhttp.open("POST", "login.php", true);
+  xhttp.open("POST", "/login.php", true);
   xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 	var uname = document.getElementById("usernameField").value;
 	var pword = document.getElementById("passwordField").value;
   xhttp.onreadystatechange = function() {
-		if (this.readyState == 4 && this.status == 200) {
-			location.replace("home.html");
-		} else if (this.readyState == 4 && this.status == 403) {
+		if (this.readyState === 4 && this.status === 200) {
+			location.replace("/home.html");
+		} else if (this.readyState === 4 && this.status === 403) {
 			document.getElementById("loginPrompt").innerHTML = "Login Failed!";
-		} else if (this.readyState == 4 && this.status >= 500) {
-			location.replace("error.html");
+		} else if (this.readyState === 4 && this.status >= 500) {
+			location.replace("/error.html");
 		}
 	};
 	xhttp.send("username="+uname+"&password="+pword);
