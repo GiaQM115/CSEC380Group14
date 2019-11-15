a639b3683f235b0d7566bbb9df4b6f18c9f1ea9e
Conrad Schneggenburger <ccs5486@rit.edu>
2019-11-15 17:48:07 -0500

    Modify the profile (upload) directory Remove upload.php Make the function of index.php (profile) HTTP-method dependent

--- docker/www/profile/index.php
+++ docker/www/profile/index.php
@@ -5,19 +5,12 @@ require_once __DIR__ . '/../src/db_conn.php';
 
 use Delight\Auth\AuthError;
 
-// Attempt to log out if the request is a POST
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    try {
-        $auth->logOut();
-        $auth->destroySession();
-    } catch (AuthError $e) {
-        http_response_code(500);
-    }
-}
-
 // Run this code if user IS logged in, otherwise redirect
 if ($auth->isLoggedIn()) {
-    // TODO
+    // Attempt to upload the video if the request is a PUT
+    if ($_SERVER['REQUEST_METHOD'] == 'PUT') {
+        // TODO
+    }
 
     // Finally, include the HTML
     include __DIR__ . '/upload.html';
